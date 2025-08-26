394559fa087a3be7c9def4093b3649534bce00eb
Arsen Yeremyan <you@example.com>
2025-08-26 12:58:44 +0400

    fonts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@use "@/assets/scss/helpers/variables.scss" as *;'
+      }
+    }
+  }
 })
